20d356cf4dbbefe50575f5e9b2f3082862ec409a
GaoKai <gkaigk1987@gmail.com>
2018-09-28 21:45:18 +0800

    新增
    增加了ElasticSearch的Maven配置，测试了打包情况，做了Thymeleaf页面测试

--- src/main/java/com/gk/controller/TestController.java
+++ src/main/java/com/gk/controller/TestController.java
@@ -1,9 +1,13 @@
 package com.gk.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -20,6 +24,12 @@ public class TestController {
 	private IThesisService thesisService;
 	
 	@RequestMapping("/")
+	public String index(Model model, HttpServletRequest request, HttpSession session) {
+		model.addAttribute("testName", "GK SpringBoot2 Test");
+		return "index";
+	}
+	
+	@RequestMapping("/jsonTest")
 	@ResponseBody
 	public JSONObject test() {
 		JSONObject json = new JSONObject();
