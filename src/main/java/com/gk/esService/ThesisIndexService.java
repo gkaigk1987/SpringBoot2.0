5e4f0bf0d299ce31b61aaf3a537dcc53f7d146fb
GaoKai <gkaigk@126.com>
2018-11-08 20:00:19 +0800

    注释掉bulk操作
    注释掉bulk操作

--- src/main/java/com/gk/esService/ThesisIndexService.java
+++ src/main/java/com/gk/esService/ThesisIndexService.java
@@ -2,7 +2,8 @@ package com.gk.esService;
 
 import java.util.List;
 
-import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.bulk.BulkRequestBuilder;
+import org.elasticsearch.action.bulk.BulkResponse;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.common.xcontent.XContentType;
 import org.slf4j.Logger;
@@ -27,9 +28,12 @@ public class ThesisIndexService {
 	private ThesisMapper thesisMapper;
 	
 	public void createThesisIndices() {
-		List<Thesis> thesisList = thesisMapper.selectByExampleWithBLOBs(new ThesisExample());
-		Thesis thesis = thesisList.get(0);
-		IndexResponse indexResponse = client.prepareIndex("user", "user").setSource(JSONObject.toJSONString(thesis),XContentType.JSON).get();
-		logger.info(JSONObject.toJSONString(indexResponse));
+//		List<Thesis> thesisList = thesisMapper.selectByExampleWithBLOBs(new ThesisExample());
+//		BulkRequestBuilder bulkRequestBuilder = client.prepareBulk();
+//		for (Thesis thesis : thesisList) {
+//			bulkRequestBuilder.add(client.prepareIndex("es", "thesis").setSource(JSONObject.toJSONString(thesis), XContentType.JSON));
+//		}
+//		BulkResponse bulkResponse = bulkRequestBuilder.get();
+//		logger.info(JSONObject.toJSONString(bulkResponse));
 	}
 }
